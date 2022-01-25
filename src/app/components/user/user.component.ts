8aa0f4ac8aea5743f5d9469dfe724a2b83f9c139
Dariusz Okonski <darek200180@gmail.com>
2022-01-25 16:32:53 +0000

    properties and method

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,5 +7,21 @@ import { Component } from "@angular/core";
 })
 
 export class UserComponent {
-    
+    firstName: string = 'John';
+    lastName: string = 'Doe';
+    age: number = 30;
+
+    constructor() {
+        console.log(this.age);
+        this.hasBirthday();
+        console.log(this.age);
+    }
+
+    sayHello() {
+        console.log(`Hello: ${this.firstName} ${this.lastName}`);
+    }
+
+    hasBirthday() {
+        this.age++;
+    }
 }
