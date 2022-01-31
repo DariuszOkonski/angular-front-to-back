bf94061b11750107717fae6beee85fefdc966140
Dariusz Okonski <darek200180@gmail.com>
2022-01-31 11:56:09 +0000

    working with ngFor

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './../../models/User';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  users: User[];
 
-  constructor() { }
+  constructor() { 
+    this.users = [];
+  }
 
   ngOnInit(): void {
-    console.log('init...');
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'Ma'
+        }
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Johnson',
+        age: 34,
+        address: {
+          street: '20 School st',
+          city: 'Lynn',
+          state: 'MA'
+        }
+      },
+      {
+        firstName: 'Karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill st',
+          city: 'Miami',
+          state: 'FL'
+        }
+      }
+    ]
+
+    this.addUser({
+      firstName: 'Dariusz',
+      lastName: 'Okonski',
+      age: 42,
+      address: {
+        street: '7 Reed Way',
+        city: 'Slough',
+        state: 'West Berkshire'
+      }
+    })
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 }
