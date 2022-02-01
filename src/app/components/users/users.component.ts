8c59f332fd863d10f7b9ffbad7ea8bbfb8fa755a
Dariusz Okonski <darek200180@gmail.com>
2022-02-01 14:40:23 +0000

    style binding & ngStyles

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -12,10 +12,12 @@ export class UsersComponent implements OnInit {
   loaded: boolean = false;
   enableAdd: boolean = true;
   currentClasses: {};
+  currentStyles: {};
 
   constructor() { 
     this.users = [];
     this.currentClasses = {};
+    this.currentStyles = {};
   }
 
   ngOnInit(): void {
@@ -76,6 +78,7 @@ export class UsersComponent implements OnInit {
     // })
 
     this.setCurrentClasses();
+    this.setCurrentStyles();
   }
 
   addUser(user: User) {
@@ -86,6 +89,13 @@ export class UsersComponent implements OnInit {
     this.currentClasses = {
       'btn-success': this.enableAdd,
       'big-text': this.showExtended,
+    }
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      'padding-top': this.showExtended ? '0' : '40px',
+      'font-size': this.showExtended ? '' : '40px'
     }
   }
 }
