63ee688e742962cc80b11af8f143be9dbd3be1d9
Dariusz Okonski <darek200180@gmail.com>
2022-02-02 16:15:47 +0000

    components and templates cleanup

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,17 +11,12 @@ export class UsersComponent implements OnInit {
   showExtended: boolean = true;
   loaded: boolean = false;
   enableAdd: boolean = true;
-  currentClasses: {};
-  currentStyles: {};
 
   constructor() { 
     this.users = [];
-    this.currentClasses = {};
-    this.currentStyles = {};
   }
 
   ngOnInit(): void {
-    // this.showExtended = false;
 
     setTimeout(() => {
       this.users = [
@@ -34,9 +29,7 @@ export class UsersComponent implements OnInit {
             city: 'Boston',
             state: 'Ma'
           },
-          image: 'https://picsum.photos/seed/picsum/600/600',
           isActive: true,
-          balance: 100,
           registered: new Date('01/02/2018 08:30:00')
         },
         {
@@ -48,9 +41,7 @@ export class UsersComponent implements OnInit {
             city: 'Lynn',
             state: 'MA'
           },
-          image: 'https://picsum.photos/600/600?grayscale',
           isActive: false,
-          balance: 200,
           registered: new Date('03/11/2017 06:20:00')
         },
         {
@@ -62,46 +53,16 @@ export class UsersComponent implements OnInit {
             city: 'Miami',
             state: 'FL'
           },
-          image: 'https://picsum.photos/600',
           isActive: true,
-          balance: 50,
           registered: new Date('11/02/2016 10:30:00')
         }
       ]
       
       this.loaded = true;
     }, 2000);
-
-    // this.addUser({
-    //   firstName: 'Dariusz',
-    //   lastName: 'Okonski',
-    //   age: 42,
-    //   address: {
-    //     street: '7 Reed Way',
-    //     city: 'Slough',
-    //     state: 'West Berkshire'
-    //   }
-    // })
-
-    this.setCurrentClasses();
-    this.setCurrentStyles();
   }
 
   addUser(user: User) {
     this.users.push(user);
-  }
-
-  setCurrentClasses() {
-    this.currentClasses = {
-      'btn-success': this.enableAdd,
-      'big-text': this.showExtended,
-    }
-  }
-
-  setCurrentStyles() {
-    this.currentStyles = {
-      'padding-top': this.showExtended ? '0' : '40px',
-      'font-size': this.showExtended ? '' : '40px'
-    }
   }
 }
