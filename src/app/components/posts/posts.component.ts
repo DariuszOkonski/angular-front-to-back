0307acf86b8e073e9a5d80a1b33737c5d19b2b2e
Dariusz Okonski <darek200180@gmail.com>
2022-02-05 20:49:10 +0000

    http client delete request

--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -52,4 +52,18 @@ export class PostsComponent implements OnInit {
 
     // })
   }
+
+  removePost(post: Post) {
+    if(confirm('Are you sure?')) {
+      this.postService.removePost(post.id).subscribe(() => {
+        console.log('deleted');
+
+        this.posts.forEach((cur, index) => {      
+          if(post.id === cur.id) {
+            this.posts.splice(index, 1);
+          }
+        })
+      })
+    }
+  }
 }
