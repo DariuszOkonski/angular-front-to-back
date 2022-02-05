fb5b2ed8df811badd12436965bf7d0261f7c88a7
Dariusz Okonski <darek200180@gmail.com>
2022-02-05 18:38:58 +0000

    rename dataService to UserService

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { User } from './../../models/User';
-import { DataService } from './../../services/data.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-users',
@@ -23,17 +23,17 @@ export class UsersComponent implements OnInit {
   data: any;
 
   constructor(
-    private dataService: DataService
+    private userService: UserService
   ) { 
     this.users = [];
   }
   
   ngOnInit(): void {
-    this.dataService.getData().subscribe(data => {
+    this.userService.getData().subscribe(data => {
       console.log(data);
     });
 
-    this.dataService.getUsers().subscribe(users => {
+    this.userService.getUsers().subscribe(users => {
       this.users = users;
       this.loaded = true;      
     })
@@ -53,7 +53,7 @@ export class UsersComponent implements OnInit {
       value.registered = new Date();
       value.hide = true;
 
-      this.dataService.addUser(value);
+      this.userService.addUser(value);
       
       this.form.reset();
     }
